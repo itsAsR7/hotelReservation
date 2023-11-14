070955ee0654c1bdd82951c242c7f0fc522ca1d9
Dilpreet Singh <dilpreet6006@gmail.com>
2023-11-13 21:43:04 -0500

    SignIn and SignUp validation and design

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,13 +1,33 @@
 import React, { useState, useEffect } from 'react';
-import { View, TextInput, Button, StyleSheet, Text } from 'react-native';
+import { Image,ImageBackground,View, TextInput, Button, StyleSheet, Text } from 'react-native';
 import { auth } from '../dbConfig';
 import { useNavigation } from '@react-navigation/native';
 import { signInWithEmailAndPassword, onAuthStateChanged } from 'firebase/auth';
 import { TouchableOpacity } from 'react-native';
 
+const backgroundImage = require('../assets/backgroundLogin.jpg');
+
+
+
+
+
 const LoginScreen = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [isValidEmail, setValidEmail] = useState(true);
+
+  const validateEmail = () => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const isValid = emailRegex.test(email);
+
+    setValidEmail(isValid);
+
+    
+  };
+
+  
+
+
 
   const navigation = useNavigation();
 
@@ -24,23 +44,46 @@ const LoginScreen = () => {
 
   return (
     <View style={styles.container}>
-      <TextInput
+      <ImageBackground source={backgroundImage} style={styles.background}>
+       
+          <View style={styles.formContainer}>
+          <Image
+          style={styles.logo}
+          source={require('../assets/HotelAppLogo.png')}
+        />
+
+
+          <TextInput
         style={styles.input}
         placeholder="Email"
         value={email}
-        onChangeText={setEmail}
+       
+        onChangeText={(text) => setEmail(text)}
+        onBlur={validateEmail}
       />
+
+      {!isValidEmail && <Text style={{ color: 'red',fontSize:'20' }}>Invalid email address</Text>}
+
+
+
+
       <TextInput
         style={styles.input}
         placeholder="Password"
+        keyboardType="email-address"
+        autoCapitalize="none"
         value={password}
         onChangeText={setPassword}
         secureTextEntry
       />
 
-      <TouchableOpacity onPress={handleLogin} style={styles.btn}>
-        <Text style={styles.btnTxt} >Login</Text>
+      <TouchableOpacity onPress={handleLogin} style={styles.loginButton}>
+        <Text style={styles.buttonText} >Login</Text>
       </TouchableOpacity>
+         
+          </View>
+        
+      </ImageBackground>
     </View>
   );
 };
@@ -48,34 +91,49 @@ const LoginScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-
-    padding: 16,
-    backgroundColor: '#fefae0', // Background color
+  },
+  background: {
+    flex: 1,
+    resizeMode: 'cover',
+    justifyContent: 'center',
+  },
+  overlay: {
+    flex: 1,
+    backgroundColor: 'rgba(0, 0, 0, 0.5)', // Adjust the overlay opacity as needed
+  },
+  formContainer: {
+    
+    
+    padding: 20,
   },
   input: {
-    width: '100%',
-    marginBottom: 10,
-    padding: 10,
-    borderWidth: 1,
-    borderColor: '#333',
+    height: 40,
+    backgroundColor: 'rgba(255, 255, 255, 0.7)', // Adjust the input background opacity as needed
+    marginBottom: 20,
+    paddingHorizontal: 10,
     borderRadius: 5,
+    marginTop:20
   },
-  btn: {
+  loginButton: {
     backgroundColor: '#e76f51',
-    borderRadius: 10,
-    paddingVertical: 10,
-    paddingHorizontal: 12,
-    alignItems: 'center',
-    justifyContent: 'center',
-    height: 55,
-    marginTop: 35,
+    paddingVertical: 15,
+    borderRadius: 5,
+    marginTop:20,
   },
-  btnTxt: {
-    fontSize: 18,
+  buttonText: {
     color: '#fff',
+    textAlign: 'center',
     fontWeight: 'bold',
-    alignSelf: 'center',
-    textTransform: 'uppercase',
+  },
+  logo: {
+    padding:50,
+    height: 200,
+    width: 200,
+    borderRadius: 100,
+    marginBottom:10,
+    marginLeft:90,
+    marginTop:10,
+
   },
 });
 
--- screens/SignupScreen.js
+++ screens/SignupScreen.js
@@ -1,18 +1,42 @@
 import React, { useState } from 'react';
-import { View, TextInput, Button, StyleSheet, TouchableOpacity, Text } from 'react-native';
+import { Image,ImageBackground,View, TextInput, Button, StyleSheet, TouchableOpacity, Text } from 'react-native';
 import { auth } from '../dbConfig';
 import { useNavigation } from '@react-navigation/native';
 import { createUserWithEmailAndPassword } from 'firebase/auth';
 
+
+const backgroundImage = require('../assets/backgroundSignUp.jpg');
+
 const SignupScreen = ( ) => {
   const [email, setEmail] = useState('');
+  const [confirmEmail, setConfirmEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [isValidEmail, setValidEmail] = useState(true);
+  const [isValidConfirmEmail, setValidConfirmEmail] = useState(true);
+
+
+  const validateEmail = () => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const isValid = emailRegex.test(email);
+
+    setValidEmail(isValid);
+
+    
+  };
+  const validateConfirmEmail = () => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const isValid = emailRegex.test(email);
+
+    setValidConfirmEmail(isValid);
+
+    
+  };
 
 
   const navigation = useNavigation();
 
   const handleSignup = async () => {
-    try {
+    try {if(email==confirmEmail){
       const user = await createUserWithEmailAndPassword(
         auth,
         email,
@@ -20,6 +44,8 @@ const SignupScreen = ( ) => {
       );
       console.log(`User ${auth.currentUser?.uid} logged in`);
       navigation.navigate('Bottom');
+      alert("Sign Up Successful");}
+      else{alert("Emails dont match")}
     } catch (err) {
       console.log(`Error when signing in user: ${err}`);
       alert("Login Error");
@@ -28,23 +54,65 @@ const SignupScreen = ( ) => {
 
   return (
     <View style={styles.container}>
-      <TextInput
+      <ImageBackground source={backgroundImage} style={styles.background}>
+       
+          <View style={styles.formContainer}>
+          <Image
+          style={styles.logo}
+          source={require('../assets/HotelAppLogo.png')}
+        />
+
+
+          <TextInput
         style={styles.input}
         placeholder="Email"
+        placeholderTextColor="#5e5e5e"
         value={email}
-        onChangeText={setEmail}
+        lowercase
+       
+        onChangeText={(text) => setEmail(text)}
+        onBlur={validateEmail}
       />
+
+      {!isValidEmail && <Text style={{ color: 'white',fontSize:'24,',fontWeight:'bold' }}>Invalid email address</Text>}
+
+      <TextInput
+        style={styles.input}
+        placeholder="Confirm Email"
+        placeholderTextColor="#5e5e5e"
+        value={confirmEmail}
+       
+        onChangeText={(text) => setConfirmEmail(text)}
+        onBlur={validateConfirmEmail}
+      />
+
+      {!isValidConfirmEmail && <Text style={{ color: 'white',fontSize:'24,',fontWeight:'bold' }}>Invalid email address</Text>}
+
+
+
+
+
+
       <TextInput
         style={styles.input}
         placeholder="Password"
+        placeholderTextColor="#5e5e5e"
+        plade
+        
+        keyboardType="email-address"
+        autoCapitalize="none"
         value={password}
         onChangeText={setPassword}
         secureTextEntry
       />
-      
-      <TouchableOpacity onPress={handleSignup} style={styles.btn}>
-        <Text style={styles.btnTxt}>Sign Up</Text>
+
+      <TouchableOpacity onPress={handleSignup} style={styles.loginButton}>
+        <Text style={styles.buttonText} >Sign Up</Text>
       </TouchableOpacity>
+         
+          </View>
+        
+      </ImageBackground>
     </View>
   );
 };
@@ -52,34 +120,51 @@ const SignupScreen = ( ) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-
-    padding: 16,
-    backgroundColor: '#fefae0', // Background color
+  },
+  background: {
+    flex: 1,
+    resizeMode: 'cover',
+    justifyContent: 'center',
+  },
+  overlay: {
+    flex: 1,
+    backgroundColor: 'rgba(0, 0, 0, 0.5)', // Adjust the overlay opacity as needed
+  },
+  formContainer: {
+    
+    
+    padding: 20,
   },
   input: {
-    width: '100%',
-    marginBottom: 10,
-    padding: 10,
-    borderWidth: 1,
-    borderColor: '#333',
+    height: 40,
+    backgroundColor: 'rgba(255, 255, 255, 0.7)', // Adjust the input background opacity as needed
+    marginBottom: 20,
+    paddingHorizontal: 10,
     borderRadius: 5,
+    marginTop:20,
+    fontSize:16,
+    
   },
-  btn: {
+  loginButton: {
     backgroundColor: '#e76f51',
-    borderRadius: 10,
-    paddingVertical: 10,
-    paddingHorizontal: 12,
-    alignItems: 'center',
-    justifyContent: 'center',
-    height: 55,
-    marginTop: 35,
+    paddingVertical: 15,
+    borderRadius: 5,
+    marginTop:20,
   },
-  btnTxt: {
-    fontSize: 18,
+  buttonText: {
     color: '#fff',
+    textAlign: 'center',
     fontWeight: 'bold',
-    alignSelf: 'center',
-    textTransform: 'uppercase',
+  },
+  logo: {
+    padding:50,
+    height: 200,
+    width: 200,
+    borderRadius: 100,
+    marginBottom:10,
+    marginLeft:90,
+    marginTop:10,
+
   },
 });
 
