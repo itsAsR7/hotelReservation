31a7c3911e5f6228cd67d6f7bcb30c43fb280ae1
itsAsR7 <anirudh.singh37@gmail.com>
2023-12-03 13:47:28 -0500

    Merge branch 'master' of https://github.com/itsAsR7/hotelReservation

---
+++ app.config.js
@@ -0,0 +1,12 @@
+export default {
+    expo: {
+      android: {
+        package: 'com.your.package.name',
+        config: {
+          appSecret: {
+            value: 'APP_CENTER_SECRET',
+          },
+        },
+      },
+    },
+  };
