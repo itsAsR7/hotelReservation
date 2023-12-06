8cd060be72fe1e7bc172a787713017ce629b479d
itsAsR7 <anirudh.singh37@gmail.com>
2023-12-05 23:55:26 -0500

    y

--- app.config.js
+++ app.config.js
@@ -1,12 +1,19 @@
 export default {
-    expo: {
-      android: {
-        package: 'com.asr7.hotelReservation',
-        config: {
-          appSecret: {
-            value: 'APP_CENTER_SECRET',
-          },
+  expo: {
+    owner: "asr7",
+    slug: "hotelReservation",
+    android: {
+      package: 'com.asr7.hotelReservation',
+      config: {
+        appSecret: {
+          value: '33b1097c-86bd-41db-b48b-9d4118c69776',
         },
       },
     },
-  };
+    extra: {
+      eas: {
+        projectId: "cfb2b419-51f1-460b-9c79-ea68b2393ab9",
+      },
+    },
+  },
+}; 
